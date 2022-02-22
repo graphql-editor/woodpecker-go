7976a34b2c83492e53e865896605ae2433b43ba7
Kamil Matysiewicz <kamil@aexol.com>
2022-02-22 11:28:00 +0100

    Improve auth

--- auth/auth_info.go
+++ auth/auth_info.go
@@ -1,15 +1,22 @@
 package auth
 
 import (
+	"encoding/base64"
+
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
 )
 
-// APIKey implements authentication using API Key on Basic Auth
+// APIKey implements authentication using API Key on Basic Auth. Expected Base64
 type APIKey string
+
+// NewAPIKey encodes api key with base64 and returns APIKey for woodpecker
+func NewAPIKey(apiKey string) APIKey {
+	return APIKey(base64.StdEncoding.EncodeToString([]byte(apiKey)))
+}
 
 // AuthenticateRequest implements  runtime.ClientAuthInfoWriter
 func (a APIKey) AuthenticateRequest(req runtime.ClientRequest, reg strfmt.Registry) error {
-	req.SetHeaderParam("Authentication", "Basic "+string(a))
+	req.SetHeaderParam("Authorization", "Basic "+string(a))
 	return nil
 }
